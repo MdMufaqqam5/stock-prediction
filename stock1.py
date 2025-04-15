277cbd2dcb27658f65d0b56c88ffc154a0dbc8a8
mohammed mufaqqam sanjar ghori <mdmufaqqam5@gmail.com>
2025-04-15 17:28:06 +0530

    Add files via upload

---
+++ stock1.py
@@ -0,0 +1,104 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import streamlit as st
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+
+
+
+st.header('Stock Market Predictor')
+
+stock =st.text_input('Enter Stock Symnbol', 'GOOG')
+start = '2022-01-01'
+end = '2025-12-31'
+
+data = yf.download(stock,start,end)
+
+st.subheader('Stock Data')
+st.write(data)
+
+data_train = pd.DataFrame(data.Close[0: int(len(data)*0.80)])
+data_test = pd.DataFrame(data.Close[int(len(data)*0.80): len(data)])
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+
+pas_100_days = data_train.tail(100)
+data_test = pd.concat([pas_100_days, data_test], ignore_index=True)
+data_test_scale = scaler.fit_transform(data_test)
+
+st.subheader('Price vs MA50')
+ma_50_days = data.Close.rolling(50).mean()
+fig1 = plt.figure(figsize=(8,6))
+plt.plot(ma_50_days, 'r')
+plt.plot(data.Close, 'g')
+plt.show()
+st.pyplot(fig1)
+
+st.subheader('Price vs MA50 vs MA100')
+ma_100_days = data.Close.rolling(100).mean()
+fig2 = plt.figure(figsize=(8,6))
+plt.plot(ma_50_days, 'r')
+plt.plot(ma_100_days, 'b')
+plt.plot(data.Close, 'g')
+plt.show()
+st.pyplot(fig2)
+
+st.subheader('Price vs MA100 vs MA200')
+ma_200_days = data.Close.rolling(200).mean()
+fig3 = plt.figure(figsize=(8,6))
+plt.plot(ma_100_days, 'r')
+plt.plot(ma_200_days, 'b')
+plt.plot(data.Close, 'g')
+plt.show()
+st.pyplot(fig3)
+
+x = []
+y = []
+
+for i in range(100, data_test_scale.shape[0]):
+    x.append(data_test_scale[i-100:i])
+    y.append(data_test_scale[i,0])
+
+x,y = np.array(x), np.array(y)
+
+from keras.layers import Dense, Dropout, LSTM
+from keras.models import Sequential
+
+model = Sequential()
+model.add(LSTM(units = 50, activation = 'relu', return_sequences = True,
+               input_shape = ((x.shape[1],1))))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units = 60, activation='relu', return_sequences = True))
+model.add(Dropout(0.3))
+
+model.add(LSTM(units = 80, activation = 'relu', return_sequences = True))
+model.add(Dropout(0.4))
+
+model.add(LSTM(units = 120, activation = 'relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(units =1))
+
+model.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+model.fit(x,y, epochs = 50, batch_size =32, verbose =1)
+
+predict = model.predict(x)
+
+scale = 1/scaler.scale_
+
+predict = predict * scale
+y = y * scale
+
+st.subheader('Original Price vs Predicted Price')
+fig4 = plt.figure(figsize=(8,6))
+plt.plot(predict, 'r', label='Original Price')
+plt.plot(y, 'g', label = 'Predicted Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+plt.show()
+st.pyplot(fig4)
